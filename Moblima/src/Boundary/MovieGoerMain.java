bfcbf2540ca770018d08e0c23f7539ddec55bf19
seahyh <yeehao_seah@hotmail.com>
2022-11-02 11:03:57 +0800

    Working code

--- Moblima/src/Boundary/MovieGoerMain.java
+++ Moblima/src/Boundary/MovieGoerMain.java
@@ -2,24 +2,18 @@ package Boundary;
 
 import Boundary.MovieGoer.DisplayBookingHistoryBoundary;
 import Boundary.MovieGoer.DisplayMovieListBoundary;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import Boundary.MovieGoer.MakeBookingBoundary;
 import Entity.Customer;
 import static Controller.CustomerController.*;
->>>>>>> main
 import static Controller.MiscMethods.*;
 import static Controller.CustomerController.*;
 import Entity.Customer;
-public class MovieGoerMain extends Boundary{
-    private boolean signedIn;
-=======
+
 import static Controller.MiscMethods.*;
 
 public class MovieGoerMain extends Boundary{
+    private boolean signedIn;
+
 	
->>>>>>> 1362c6aa0af163a7eaafc90ca78fd36bb3e4c11d
     @Override
     protected void start(){
         displayMovieGoerView();
@@ -38,29 +32,13 @@ public class MovieGoerMain extends Boundary{
         switch (choice) {
             case 1:
                 direct(this, new DisplayMovieListBoundary());
-<<<<<<< HEAD
                 end();
                 break;
             case 2:
             	direct(this, new DisplayBookingHistoryBoundary());
-=======
->>>>>>> main
-                end();
-                break;
-<<<<<<< HEAD
-            case 3:
-<<<<<<< HEAD
-=======
-            	//direct(this, new DisplayBookingHistoryBoundary());
-                //direct(this, new MovieGoerMain());
-=======
-            case 2:
-            	direct(this, new DisplayBookingHistoryBoundary());
->>>>>>> 1362c6aa0af163a7eaafc90ca78fd36bb3e4c11d
                 end();
                 break;
             case 3:
->>>>>>> main
             	end();
                 break;
         }
